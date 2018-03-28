d0b232306d0d56d9e6271ceb3b58e40a78487c05
Nicholas Wang <nwang@e1z3r4p4.42.us.org>
2018-03-28 14:43:38 -0700

    a

--- listener.c
+++ listener.c
@@ -3,7 +3,7 @@
 #include<unistd.h>
 #include<stdio.h>
 #include<fcntl.h>
-#include "../libft/libft.h"
+#include "libft/libft.h"
 
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	fd1 = open("speech.txt", O_RDWR | O_TRUNC);
 	dup2(fd1, 1);
-	system("pocketsphinx_continuous -inmic yes -lm dict/dict.lm \
-			-dict dict/dict.dic -logfn /dev/null");
+	system("pocketsphinx_continuous -inmic yes -hmm papidict/en-us-papi -lm papidict/dict.lm \
+			-dict papidict/dict.dic -logfn /dev/null");
 	return (0);
 }
