62218fb12ab40b027d5cb1487e103716b951024b
Nicholas Wang <nwang@e1z2r9p16.42.us.org>
2018-03-28 23:00:58 -0700

    commands

---
+++ commands/run_commands2.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run_commands.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nwang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/28 22:38:43 by nwang             #+#    #+#             */
+/*   Updated: 2018/03/28 22:58:56 by nwang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	lights_on(void)
+{
+	system("sh ./script/maxlights.script")
+//	t2s ("Turning lights on");
+}
+
+void	lights_off(void)
+{
+	system("sh ./script/maxlights.script")
+//	t2s ("Turning lights off");
+}
+
+void	set_alarm(void)
+{
+
+}
+
+void	set_alarm(void)
+{
+
+}
+
+void	set_alarm(void)
+{
+
+}
